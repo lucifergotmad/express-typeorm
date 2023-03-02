0f2955f5c9495f7f2f01af5289a6cd24afdcef51
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-03-02 15:28:15 +0700

    fix:[minor] update error handler

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,15 +1,20 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import status from "http-status";
 
 import { env } from "../config/env-schema";
 
-const errorHandler = (error: Error, req: Request, res: Response) => {
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const statusCode = res.statusCode ?? status.INTERNAL_SERVER_ERROR;
 
   res.status(statusCode);
   res.json({
-    message: error.message,
-    stack: env.MODE_API === "production" ? null : error.stack,
+    message: err.message,
+    stack: env.MODE_API === "production" ? null : err.stack,
   });
 };
 
